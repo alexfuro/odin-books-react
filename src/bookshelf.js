474c862b138c453145960e679172e4d67a617cb7
AlexFuro <afurones43@gmail.com>
2018-07-01 20:56:01 -0400

    Add sample data and fix table structure

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import Book from './book';
 
+
+const sample = [
+  {
+    title: "Moby Dick",
+    author: "Some dude",
+    descrip: "Whales man",
+    pages: "2000",
+    status: true
+  },{
+    title: "Dick Tracy",
+    author: "Some other dude",
+    descrip: "Dectective man",
+    pages: "200",
+    status: false
+  },{
+    title: "No Longer Human",
+    author: "Dazai",
+    descrip: "Jap man",
+    pages: "500",
+    status: false
+  }
+];
+
 class BookShelf extends Component {
   constructor(props){
     super(props);
-    this.state = {books: []};
+    this.state = {books: sample};
   }
   render() {
     const books = this.state.books;
     return (
       <table>
         <thead>
-          <th>
-            <td>Title</td>
-            <td>Author</td>
-            <td>Description</td>
-            <td>Pages</td>
-            <td>Read Status</td>
-            <td><button>Change Status</button></td>
-            <td><button>Delete Book</button></td>
-          </th>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Description</th>
+            <th>Pages</th>
+            <th>Read Status</th>
+            <th>Read/Unread</th>
+            <th>Remove</th>
+          </tr>
         </thead>
         <tbody>
           {books.map((book,index)=>(
